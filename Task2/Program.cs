c4fed5f82432d6d2321f5520a49760089ae8ede4
Dimase <zwere4ek@list.ru>
2024-02-01 16:02:45 +0300

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string originalString = "Hello World! This Is A Test String.";
+
+        string resultString = ConvertToUpper(originalString);
+
+        Console.WriteLine("Исходная строка: " + originalString);
+        Console.WriteLine("Результат: " + resultString);
+    }
+
+    static string ConvertToUpper(string input)
+    {
+        char[] charArray = input.ToCharArray();
+
+        for (int i = 0; i < charArray.Length; i++)
+        {
+            if (char.IsUpper(charArray[i]))
+            {
+                charArray[i] = char.ToLower(charArray[i]);
+            }
+        }
+
+        return new string(charArray);
+    }
+}
