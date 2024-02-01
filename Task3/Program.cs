93dd7041c0b0101e5f57e0d993d52a9ba0602bbd
Dimase <zwere4ek@list.ru>
2024-02-01 16:03:10 +0300

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = "level"; // Произвольная строка
+
+        bool isPalindrome = IsPalindrome(input);
+
+        if (isPalindrome)
+        {
+            Console.WriteLine($"Строка \"{input}\" является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine($"Строка \"{input}\" не является палиндромом.");
+        }
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        int length = str.Length;
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (str[i] != str[length - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
