df207a90fe951dd1c852ad59cc186f7672c7115c
Dimase <zwere4ek@list.ru>
2024-02-01 16:02:15 +0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char[,] charArray = {
+            {'a', 'b', 'c'},
+            {'d', 'e', 'f'},
+            {'g', 'h', 'i'}
+        };
+
+        string str = GetStringFromCharArray(charArray);
+
+        Console.WriteLine("Строка из символов массива: " + str);
+    }
+    static string GetStringFromCharArray(char[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+
+        string result = "";
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                result += array[i, j];
+            }
+        }
+
+        return result;
+    }
+}
